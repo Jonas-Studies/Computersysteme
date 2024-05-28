ac18fe2788f9ffb225a5dc2697280da5c08c9138
Jonas-Laptop <studies@jonas.zengerle.email>
2024-05-28 15:10:32 +0200

    Hinzufügen der Lösung für U7.1

--- Übungen/Übung_7/Lösung_Jonas_und_Marc/U7_1.s
+++ Übungen/Übung_7/Lösung_Jonas_und_Marc/U7_1.s
@@ -1,14 +1,60 @@
 .section .rodata
-
-.section .bss
+	OUTPUT_DECIMAL:
+		.asciz "%ld"
+	OUTPUT_SPACE:
+		.asciz " "
+	OUTPUT_BLANKS:
+		.asciz ""
 
 .section .text
 	.globl main
 	.type main, @function
 
+	.equ LIMIT, 3
+
 main:
 	pushq %rbp
 	movq %rsp, %rbp
+
+	movq $1, %r14
+
+do_while_k_isNotGreater_LIMIT:
+	movq $1, %r12
+
+	jmp while_i_isNotGreater_LIMIT
+
+do_for_i_isNotGreater_LIMIT:
+	movq $1, %r13
+
+	jmp check_if_j_isNotGreater_LIMIT
+
+while_j_isNotGreater_LIMIT_do:
+	movq $OUTPUT_DECIMAL, %rdi
+	movq %r13, %rsi
+	call printf
+
+	add $1, %r13
+
+check_if_j_isNotGreater_LIMIT:
+	cmp $LIMIT, %r13
+	jbe while_j_isNotGreater_LIMIT_do
+
+	movq $OUTPUT_SPACE, %rdi
+	call printf
+
+	addq $1, %r12
+	
+while_i_isNotGreater_LIMIT:
+	cmp $LIMIT, %r12
+	jbe do_for_i_isNotGreater_LIMIT
+
+	addq $1, %r14
+
+	movq $OUTPUT_BLANKS, %rdi
+	call puts
+
+	cmp $LIMIT, %r14
+	jbe do_while_k_isNotGreater_LIMIT
 
 	movq $0, %rax
 	popq %rbp
